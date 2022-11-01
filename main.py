dc2ee62fc43f220373e83ed35a0f6bd54566bd2e
ZILIN YE <yezilin2014@outlook.com>
2022-11-01 18:15:29 -0400

    init

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from email import header
 import threading
 import requests
 import json
@@ -6,81 +5,84 @@ from maria import Maria
 from tqdm import tqdm
 from queue import Queue
 import math
-exitFlag=0
+import zipfile
+import os
+from PyPDF2 import PdfMerger
+exitFlag = 0
 studentInfo = Maria()
-token = "1000.2d7cd1a6d206633bb3a67775c3a6fd8d.4fa07f006931def905ed8a1cc88fe51a"
+token = "1000.cdee825c8296f65377fed992fbe24a33.3c5e94cc1af893539abc58d3e44082cb"
 url = "https://sign.zoho.com/api/v1/requests"
 headers = {
-        "Authorization": "Zoho-oauthtoken "+ token,
-    }
+    "Authorization": "Zoho-oauthtoken " + token,
+}
 row_count = 10
+
 
 def HttpRequest(start_index):
     params = {
-        "data" : '{"page_context": {"row_count":'+str(row_count)+' , "start_index": '+str(start_index)+', "search_columns": {"request_name": "22F"}, "sort_column": "created_time", "sort_order": "DESC"}}'
+        "data": '{"page_context": {"row_count":'+str(row_count)+' , "start_index": '+str(start_index)+', "search_columns": {"request_name": "22F"}, "sort_column": "created_time", "sort_order": "DESC"}}'
     }
     r = requests.get(url, headers=headers, params=params).json()
     return r
 
+
 qsize = HttpRequest(1)['page_context']['total_count']
 queueLock = threading.Lock()
 DownloadList = Queue(qsize)
-thread_number = 10
+ExtractList = Queue(qsize)
+thread_number = 2
+
 
 class myThread (threading.Thread):
-    def __init__(self,threadID,q):
+    def __init__(self, threadID, q):
         threading.Thread.__init__(self)
         self.threadID = threadID
 
         self.q = q
+
     def run(self):
         print("starting thread "+str(self.threadID))
-        getDownloadPDF(self.q)
+        pdfProcess(self.q)
         print("finished thread "+str(self.threadID))
-        
-        
 
 
-
-
-    
-    
-# grab document list from the API that mathch the search 
+# grab document list from the API that mathch the search
 def getDocumentList(start_index) -> list:
 
     outputlist = []
     documentlist = HttpRequest(start_index)['requests']
     for item in documentlist:
         if item['request_status'] == 'completed':
-            
+
             campemail = item['actions'][0]['recipient_email']
-            
+
             fileprefix = item['document_ids'][0]['document_name'][:9]
-            
+
             request_id = item['request_id']
-            
+
             studentID = getStudentInfo(campemail=campemail)
             if studentID:
 
-                outputlist.append([fileprefix,request_id,studentID])
+                outputlist.append([fileprefix, request_id, studentID])
             else:
                 print('%s email not found in Database' % campemail)
-                
+
     # print(len(outputlist))
     # print(outputlist)
     return outputlist
-    
 
 
 # Get student information from database
 def getStudentInfo(campemail) -> str:
     studentID = studentInfo.GetStudentInfo(campemail=campemail)
     return studentID
-    
+
 # Download zip file
+
+
 def getDownloadPDF(q):
     # while not exitFlag:
-        
+
     #     if not DownloadList.empty():
     #         queueLock.acquire()
     #         data = q.get()
@@ -99,61 +101,75 @@ def getDownloadPDF(q):
     #                     progress_bar.update(len(chunk))
     #                     f.write(chunk)
     #         progress_bar.close()
- 
 
-    fileprefix=q[0]
+    fileprefix = q[0]
     request_id = q[1]
     student_id = q[2]
     # print(data)
-    r = requests.get(url+"/"+request_id+"/pdf",headers=headers,stream=False)
-    total_size_in_bytes= int(r.headers.get('content-length', 0))
-    block_size = 1024 #1 Kibibyte
+    r = requests.get(url+"/"+request_id+"/pdf", headers=headers, stream=False)
+    total_size_in_bytes = int(r.headers.get('content-length', 0))
+
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-    with open(fileprefix+student_id+'.zip',"wb") as f:
+    zipfilename = fileprefix+student_id
+    with open(zipfilename+'.zip', "wb") as f:
         for chunk in r.iter_content(chunk_size=10240):
             if chunk:  # filter out keep-alive new chunks
                 progress_bar.update(len(chunk))
                 f.write(chunk)
     progress_bar.close()
+    ExtractList.put(zipfilename)
+
+
+def pdfProcess(q):
+
+    if not ExtractList.empty():
+        queueLock.acquire()
+        filename = q.get()
+        
+        with zipfile.ZipFile(filename+'.zip', 'r') as zip_ref:
+            zip_ref.extractall(filename)
+        merger = PdfMerger()
+        for item in os.listdir(filename):
+            merger.append(filename+"\\"+item)
+        merger.write(filename+".pdf")
+        merger.close()
+        os.remove(filename+'.zip')
+        queueLock.release()
 
 
 def main():
 
-    # threads=[]
+    threads = []
 
-    # thread_index = 1
+    thread_index = 1
     start_index = 1
 
-    # while thread_index <= thread_number:
-    #     thread = myThread(thread_index,DownloadList)
-    #     thread.start()
-    #     threads.append(thread)
-    #     thread_index +=1
-    # i = 1
-    # while i <= thread_number:
         
-    #     pageList = getDocumentList(start_index)
-    #     for item in pageList:
-    #         DownloadList.put(item)
-    #     start_index += 10
-    #     i+=1
-    dlist  = []
+    dlist = []
     i = 1
     while i <= thread_number:
-        
+
         pageList = getDocumentList(start_index)
         for item in pageList:
             dlist.append(item)
         start_index += 10
-        i+=1
+        i += 1
     for item in dlist:
         getDownloadPDF(item)
-    # while not DownloadList.empty():
-    #     pass
-    # exitFlag = 1 
+
+    while not ExtractList.empty():
+        pdfProcess(ExtractList)
+    # while thread_index <= thread_number:
+    #     thread = myThread(thread_index, ExtractList)
+    #     thread.start()
+    #     threads.append(thread)
+    #     thread_index += 1
+
+
+    # exitFlag = 1
     # for t in threads:
     #     t.join()
-    # print("exit the thread!!")
+    print("exit the thread!!")
 
 
 main()

5a0d4c30d6677cc2c10dcfdedfbf6d3491e4a042
ZILIN YE <yezilin2014@outlook.com>
2022-11-01 17:28:07 -0400

    init

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from queue import Queue
 import math
 exitFlag=0
 studentInfo = Maria()
-token = "1000.468a50275f341b0866c18fd8afa2f78e.951c90bb336cab21dae178dc7ec7858d"
+token = "1000.2d7cd1a6d206633bb3a67775c3a6fd8d.4fa07f006931def905ed8a1cc88fe51a"
 url = "https://sign.zoho.com/api/v1/requests"
 headers = {
         "Authorization": "Zoho-oauthtoken "+ token,
@@ -79,56 +79,81 @@ def getStudentInfo(campemail) -> str:
     
 # Download zip file
 def getDownloadPDF(q):
-    while not exitFlag:
+    # while not exitFlag:
         
-        if not DownloadList.empty():
-            queueLock.acquire()
-            data = q.get()
-            queueLock.release()
-            fileprefix=data[0]
-            request_id = data[1]
-            student_id = data[2]
-            # print(data)
-            r = requests.get(url+"/"+request_id+"/pdf",headers=headers,stream=True)
-            total_size_in_bytes= int(r.headers.get('content-length', 0))
-            block_size = 1024 #1 Kibibyte
-            progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-            with open(fileprefix+student_id+'.zip',"wb") as f:
-                for chunk in r.iter_content(chunk_size=10240):
-                    if chunk:  # filter out keep-alive new chunks
-                        progress_bar.update(len(chunk))
-                        f.write(chunk)
-            progress_bar.close()
-        
+    #     if not DownloadList.empty():
+    #         queueLock.acquire()
+    #         data = q.get()
+    #         queueLock.release()
+    #         fileprefix=data[0]
+    #         request_id = data[1]
+    #         student_id = data[2]
+    #         # print(data)
+    #         r = requests.get(url+"/"+request_id+"/pdf",headers=headers,stream=False)
+    #         total_size_in_bytes= int(r.headers.get('content-length', 0))
+    #         block_size = 1024 #1 Kibibyte
+    #         progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    #         with open(fileprefix+student_id+'.zip',"wb") as f:
+    #             for chunk in r.iter_content(chunk_size=10240):
+    #                 if chunk:  # filter out keep-alive new chunks
+    #                     progress_bar.update(len(chunk))
+    #                     f.write(chunk)
+    #         progress_bar.close()
+ 
+
+    fileprefix=q[0]
+    request_id = q[1]
+    student_id = q[2]
+    # print(data)
+    r = requests.get(url+"/"+request_id+"/pdf",headers=headers,stream=False)
+    total_size_in_bytes= int(r.headers.get('content-length', 0))
+    block_size = 1024 #1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(fileprefix+student_id+'.zip',"wb") as f:
+        for chunk in r.iter_content(chunk_size=10240):
+            if chunk:  # filter out keep-alive new chunks
+                progress_bar.update(len(chunk))
+                f.write(chunk)
+    progress_bar.close()
 
 
 def main():
 
-    threads=[]
+    # threads=[]
 
-    thread_index = 1
+    # thread_index = 1
     start_index = 1
 
-    while thread_index <= thread_number:
-        thread = myThread(thread_index,DownloadList)
-        thread.start()
-        threads.append(thread)
-        thread_index +=1
+    # while thread_index <= thread_number:
+    #     thread = myThread(thread_index,DownloadList)
+    #     thread.start()
+    #     threads.append(thread)
+    #     thread_index +=1
+    # i = 1
+    # while i <= thread_number:
+        
+    #     pageList = getDocumentList(start_index)
+    #     for item in pageList:
+    #         DownloadList.put(item)
+    #     start_index += 10
+    #     i+=1
+    dlist  = []
     i = 1
     while i <= thread_number:
         
         pageList = getDocumentList(start_index)
         for item in pageList:
-            DownloadList.put(item)
+            dlist.append(item)
         start_index += 10
         i+=1
-
-    while not DownloadList.empty():
-        pass
-    exitFlag = 1 
-    for t in threads:
-        t.join()
-    print("exit the thread!!")
+    for item in dlist:
+        getDownloadPDF(item)
+    # while not DownloadList.empty():
+    #     pass
+    # exitFlag = 1 
+    # for t in threads:
+    #     t.join()
+    # print("exit the thread!!")
 
 
 main()
--- test.py
+++ test.py
@@ -1,19 +1,3 @@
-import requests
-from tqdm import tqdm
-
-
-
-token = "1000.2d7cd1a6d206633bb3a67775c3a6fd8d.4fa07f006931def905ed8a1cc88fe51a"
-url = "https://sign.zoho.com/api/v1/requests"
-headers = {
-        "Authorization": "Zoho-oauthtoken "+ token,
-    }
-r = requests.get(url+"/"+'130808000003585129'+"/pdf",headers=headers,stream=True)
-total_size_in_bytes= int(r.headers.get('content-length', 0))
-block_size = 1024 #1 Kibibyte
-progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-with open('test.zip',"wb") as f:
-    for chunk in r.iter_content(chunk_size=1024):
-        if chunk:  # filter out keep-alive new chunks
-            progress_bar.update(len(chunk))
-            f.write(chunk)
+import zipfile
+with zipfile.ZipFile('test.zip', 'r') as zip_ref:
+    zip_ref.extractall('test')
